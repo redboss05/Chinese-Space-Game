0725fcef32b450a0405b1cc01f77393b17b3ce7c
Redboss05 <debrossedeen@gmail.com>
2023-12-24 20:37:45 -0500

    Alpha version

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+words = {"good":"好","I":"我","you":"你"}
+word_list = []
+for word in words.keys():
+    word_list.append(word)
+
+test_words = {"good":"好","I":"我","you":"你"}
+test_word_list = []
+for word in test_words.keys():
+    test_word_list.append(word)
