14b4fccc34c34ab0c8e060c28b3c31b60eb5d9e4
Redboss05 <debrossedeen@gmail.com>
2023-12-27 02:02:02 -0500

    Alpha version

--- chinese_space_game.py
+++ chinese_space_game.py
@@ -2,6 +2,7 @@ import pygame
 from sys import exit
 import random
 from dictionary import words, word_list
+from stars import star_list
 """
 Maybe a rocket heading towards USsss
 """
@@ -20,42 +21,61 @@ def place_word(words, word_list, font):
     rand_choices = []
     x=0
     for choice in [magic_choice, bum_choice1, bum_choice2]:
-        rand_choices.insert(random.randint(0, len(rand_choices)), words[choice])
+        rand_choices.insert(random.randint(0, len(rand_choices)), words[choice][0])
         x += 1
 
+    rand_words = []
+    for rand_choice in rand_choices:
+        for choice in [magic_choice, bum_choice1, bum_choice2]:
+            if rand_choice == words[choice][0]:
+                rand_words.append(words[choice][1])
+
     print(rand_choices)
-    print(words[magic_choice])
+    print(words[magic_choice][0])
     for i, choice in enumerate(rand_choices):
-        if choice == words[bum_choice1]:
+        if choice == words[bum_choice1][0]:
             x_pos1 = i
-        elif choice == words[bum_choice2]:
+        elif choice == words[bum_choice2][0]:
             x_pos2 = i
-    return(magic_choice, rand_choices, x_pos1, x_pos2)
+    return(magic_choice, rand_choices, x_pos1, x_pos2, rand_words)
+
+def stars(spawned, star_list):
+    if spawned == False:
+        for star in star_list:
+            star.append(star[0].get_rect(center=(random.randrange(400),random.randrange(-100,800))))
+    for star in star_list:
+        screen.blit(star[0],star[1])
+        pygame.draw.rect(screen, "White", star[1])
+        star[1].y += 10
+        if star[1].y > 800:
+            star[1].x = random.randrange(400)
+            star[1].y = random.randrange(-200,0)
 
 def spawn_lasers(pos1, pos2):
     if pos1 == 0:
-        a = 50
+        a = 65
     elif pos1 == 1:
-        a = 183
+        a = 190
     else:
-        a = 316
+        a = 335
 
     if pos2 == 0:
-        b = 50
+        b = 65
     elif pos2 == 1:
-        b = 183
+        b = 190
     else:
-        b = 316
+        b = 335
 
-    laser_surf1 = pygame.Surface((133.33, 800))
+    laser_surf1 = pygame.Surface((145, 800))
     laser_rect1 = laser_surf1.get_rect(center = (a,-1600))
-    laser_surf2 = pygame.Surface((133.33, 800))
+    laser_surf2 = pygame.Surface((145, 800))
     laser_rect2 = laser_surf2.get_rect(center=(b, -1600))
     return([laser_surf1,laser_rect1,laser_surf2,laser_rect2])
 
 
 word_font = pygame.font.Font(None, 50)
 char_font = pygame.font.Font("chinese.stsong.ttf", 50)
+pronounced_font = pygame.font.Font(None, 25)
 screen = pygame.display.set_mode((400, 800)) #initializes display window
 pygame.display.set_caption("Chinese Space Game")
 clock = pygame.time.Clock() #initializes clock object
@@ -70,6 +90,7 @@ rand_chars = []
 lasers = []
 move_left = False
 move_right = False
+spawned = False
 while True:
     for event in pygame.event.get(): #gets all possible events
         if event.type == pygame.QUIT:
@@ -85,13 +106,13 @@ while True:
             move_left = False
             move_right = False
         if event.type == word_timer:
-            magic_word, rand_chars, x_pos1, x_pos2 = place_word(words, word_list, word_font)
-            print(x_pos1, x_pos2)
+            magic_word, rand_chars, x_pos1, x_pos2, rand_words = place_word(words, word_list, word_font)
+            print(rand_words)
             lasers = spawn_lasers(x_pos1, x_pos2)
             #print(rand_chars)
             #print(magic_word)
 
-    screen.fill("Blue")
+    screen.fill("Black")
 
     if move_left:
         if not player_rect.x < 0:
@@ -102,22 +123,31 @@ while True:
 
     screen.blit(player_surf, player_rect)
     pygame.draw.rect(screen, "Pink", player_rect)
-    pygame.draw.polygon(screen, "Red", [(player_rect.centerx-25,player_rect.centery+25),(player_rect.centerx,player_rect.centery-25),(player_rect.centerx + 25,player_rect.centery+25)])
+    pygame.draw.polygon(screen, "White", [(player_rect.centerx-25,player_rect.centery+25),(player_rect.centerx,player_rect.centery-25),(player_rect.centerx + 25,player_rect.centery+25)])
 
-    word_surf = word_font.render(magic_word, False, "Black")
+    word_surf = word_font.render(magic_word, False, "White")
     word_rect = word_surf.get_rect(center=(200, 400))
     screen.blit(word_surf, word_rect)
 
+    stars(spawned, star_list)
+    spawned = True
+
     x = 50
     if rand_chars:
-        for char in rand_chars:
-            screen.blit(char_font.render(char, False, "Black"), (x, 60))
-            x += 133
-        x = 50
+        for i, char in enumerate(rand_chars):
+            char_surf = char_font.render(char, False, "White")
+            char_rect = char_surf.get_rect(center=(x,60))
+            pronounced_surf = pronounced_font.render(rand_words[i], False, "White")
+            pronounced_rect = pronounced_surf.get_rect(center=(x, 30))
+            screen.blit(char_surf,char_rect)
+            screen.blit(pronounced_surf,pronounced_rect)
+            x += 150
 
     if lasers:
         screen.blit(lasers[0], lasers[1])
+        pygame.draw.rect(screen, "Red", lasers[1])
         screen.blit(lasers[2], lasers[3])
+        pygame.draw.rect(screen, "Red", lasers[3])
         lasers[1].y += 10
         lasers[3].y += 10
         if lasers[1].colliderect(player_rect) or lasers[3].colliderect(player_rect):
--- dictionary.py
+++ dictionary.py
@@ -1,9 +1,38 @@
-words = {"good":"好","I":"我","you":"你"}
+words = {"GOOD":["好","hao3"],
+         "I":["我","wo3"],
+         "YOU":["你","ni3"],
+         "SEE":["见","jian4"],
+         "AGAIN":["再","zai4"],
+         "GOODBYE":["再见","zai4jian4"],
+         "HAPPY":["高兴","gao1xing4"],
+         "ENGLISH":["英语","ying1yu3"],
+         "CHINESE":["汉语","han4yu3"],
+         "SPEAK":["说","shuo1"],
+         "HELLO":["你好","ni3hao3"],
+         "CALL":["叫","jiao4"],
+         "LAST NAME":["姓","xing4"],
+         "HE":["他","ta1"],
+         "NO":["不","bu4"],
+         "SHE":["她","ta1"],
+         "?":["吗","ma1"],
+         "LANGUAGE":["语","yu3"],
+         "ALL":["都","dou1"],
+         "LEARN":["学","xue2"],
+         "BE":["是","shi4"],
+         "WRITE":["写","xie3"],
+         "EAT":["吃","chi1"],
+         "AND":["和","he2"],
+         "APPLE":["苹果","ping2guo3"],
+         "BREAD":["面包","mian4bao1"],
+         "EGG":["鸡蛋","ji1dan4"],
+         "FRUIT":["水果","shui3guo3"],
+         "LIKE":["喜欢","xi3huan5"],
+         "TEA":["茶","cha2"]}
 word_list = []
 for word in words.keys():
     word_list.append(word)
 
-test_words = {"good":"好","I":"我","you":"你"}
-test_word_list = []
-for word in test_words.keys():
-    test_word_list.append(word)
+xwords = {"good":["好"],"I":["我"],"you":["你"],"hello":["你好"]}
+xword_list = []
+for word in xwords.keys():
+    xword_list.append(word)
