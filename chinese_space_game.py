765331d442c9cfe9b2a254583a0c4fbe8f979310
Redboss05 <debrossedeen@gmail.com>
2023-12-29 00:11:45 -0500

    Start screen added

--- chinese_space_game.py
+++ chinese_space_game.py
@@ -30,8 +30,6 @@ def place_word(words, word_list, font):
             if rand_choice == words[choice][0]:
                 rand_words.append(words[choice][1])
 
-    print(rand_choices)
-    print(words[magic_choice][0])
     for i, choice in enumerate(rand_choices):
         if choice == words[bum_choice1][0]:
             x_pos1 = i
@@ -77,7 +75,7 @@ word_font = pygame.font.Font(None, 50)
 char_font = pygame.font.Font("chinese.stsong.ttf", 50)
 pronounced_font = pygame.font.Font(None, 25)
 screen = pygame.display.set_mode((400, 800)) #initializes display window
-pygame.display.set_caption("Chinese Space Game")
+pygame.display.set_caption("Rocket Chinese")
 clock = pygame.time.Clock() #initializes clock object
 word_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(word_timer,5000)
@@ -91,6 +89,7 @@ lasers = []
 move_left = False
 move_right = False
 spawned = False
+game_running = False
 while True:
     for event in pygame.event.get(): #gets all possible events
         if event.type == pygame.QUIT:
@@ -102,56 +101,83 @@ while True:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 move_left = True
+        if event.type == pygame.KEYDOWN:
+            game_running = True
         if event.type == pygame.KEYUP:
             move_left = False
             move_right = False
         if event.type == word_timer:
-            magic_word, rand_chars, x_pos1, x_pos2, rand_words = place_word(words, word_list, word_font)
-            print(rand_words)
-            lasers = spawn_lasers(x_pos1, x_pos2)
-            #print(rand_chars)
-            #print(magic_word)
+            if game_running:
+                magic_word, rand_chars, x_pos1, x_pos2, rand_words = place_word(words, word_list, word_font)
+                lasers = spawn_lasers(x_pos1, x_pos2)
+                #print(rand_chars)
+                #print(magic_word)
 
     screen.fill("Black")
-
-    if move_left:
-        if not player_rect.x < 0:
-            player_rect.x -= 4
-    if move_right:
-        if not player_rect.x > 385:
-            player_rect.x += 4
-
-    screen.blit(player_surf, player_rect)
-    pygame.draw.rect(screen, "Pink", player_rect)
-    pygame.draw.polygon(screen, "White", [(player_rect.centerx-25,player_rect.centery+25),(player_rect.centerx,player_rect.centery-25),(player_rect.centerx + 25,player_rect.centery+25)])
-
-    word_surf = word_font.render(magic_word, False, "White")
-    word_rect = word_surf.get_rect(center=(200, 400))
-    screen.blit(word_surf, word_rect)
-
     stars(spawned, star_list)
-    spawned = True
 
-    x = 50
-    if rand_chars:
-        for i, char in enumerate(rand_chars):
-            char_surf = char_font.render(char, False, "White")
-            char_rect = char_surf.get_rect(center=(x,60))
-            pronounced_surf = pronounced_font.render(rand_words[i], False, "White")
-            pronounced_rect = pronounced_surf.get_rect(center=(x, 30))
-            screen.blit(char_surf,char_rect)
-            screen.blit(pronounced_surf,pronounced_rect)
-            x += 150
+    if game_running == True:
 
-    if lasers:
-        screen.blit(lasers[0], lasers[1])
-        pygame.draw.rect(screen, "Red", lasers[1])
-        screen.blit(lasers[2], lasers[3])
-        pygame.draw.rect(screen, "Red", lasers[3])
-        lasers[1].y += 10
-        lasers[3].y += 10
-        if lasers[1].colliderect(player_rect) or lasers[3].colliderect(player_rect):
-             pygame.quit()
-             exit()
+        if move_left:
+            if not player_rect.x < 0:
+                player_rect.x -= 4
+        if move_right:
+            if not player_rect.x > 385:
+                player_rect.x += 4
+
+        screen.blit(player_surf, player_rect)
+        pygame.draw.rect(screen, "Pink", player_rect)
+        pygame.draw.polygon(screen, "White", [(player_rect.centerx-25,player_rect.centery+25),(player_rect.centerx,player_rect.centery-25),(player_rect.centerx + 25,player_rect.centery+25)])
+
+        word_surf = word_font.render(magic_word, False, "White")
+        word_rect = word_surf.get_rect(center=(200, 400))
+        screen.blit(word_surf, word_rect)
+
+        #stars(spawned, star_list)
+        #spawned = True
+
+        x = 50
+        if rand_chars:
+            for i, char in enumerate(rand_chars):
+                char_surf = char_font.render(char, False, "White")
+                char_rect = char_surf.get_rect(center=(x,60))
+                pronounced_surf = pronounced_font.render(rand_words[i], False, "White")
+                pronounced_rect = pronounced_surf.get_rect(center=(x, 30))
+                screen.blit(char_surf,char_rect)
+                screen.blit(pronounced_surf,pronounced_rect)
+                x += 150
+
+        if lasers:
+            screen.blit(lasers[0], lasers[1])
+            pygame.draw.rect(screen, "Red", lasers[1])
+            screen.blit(lasers[2], lasers[3])
+            pygame.draw.rect(screen, "Red", lasers[3])
+            lasers[1].y += 10
+            lasers[3].y += 10
+            if lasers[1].colliderect(player_rect) or lasers[3].colliderect(player_rect):
+                 game_running = False
+                 lasers[1].x = 1000
+                 lasers[3].x = 1000
+                 magic_word = None
+                 for char in rand_chars[:]:
+                     rand_chars.remove(char)
+                 print(rand_chars)
+
+    else:
+
+        title_surf = word_font.render("Rocket Chinese", False, "White")
+        title_rect = title_surf.get_rect(center=(200, 50))
+        screen.blit(title_surf, title_rect)
+
+        subtext_surf = pronounced_font.render("press any button to start", False, "White")
+        subtext_rect = subtext_surf.get_rect(center=(200, 100))
+        screen.blit(subtext_surf, subtext_rect)
+
+        pygame.draw.polygon(screen, "White", [(player_rect.centerx - 25, player_rect.centery + 25),
+                                              (player_rect.centerx, player_rect.centery - 25),
+                                              (player_rect.centerx + 25, player_rect.centery + 25)])
+
+
+
     pygame.display.update() #updates pygame
     clock.tick(60) #this loop does not runnen faster than 60 frames
